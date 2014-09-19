82ec78ad3fa285be311a12cc68a82b4a32590b57
Mathias Verraes <mathias@verraes.net>
2014-09-19 17:42:19 +0200

    fix accidental deletion

--- src/Model/P1_ValueObjects/Lesson.php
+++ src/Model/P1_ValueObjects/Lesson.php
@@ -8,12 +8,18 @@ final class Lesson
     private $teacher;
     private $subject;
 
-    public function __construct(WeeklyTimeSlot $weeklyTimeSlot, Subject $subject, TeacherId $teacher))
+    public function __construct(WeeklyTimeSlot $weeklyTimeSlot, Subject $subject, TeacherId $teacher)
     {
         $this->weeklyTimeSlot = $weeklyTimeSlot;
         $this->subject = $subject;
         $this->teacher = $teacher;
     }
 
-
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf("%s %s with %s", $this->weeklyTimeSlot, $this->subject, $this->teacher);
+    }
 }
